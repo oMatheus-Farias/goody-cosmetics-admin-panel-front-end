92076f91a4b4e7e3ae21bc35a999b6038d61ddf9
Matheus <matheusfariasdasneves@gmail.com>
2025-04-10 16:46:50 -0300

    feat: add MainContentHeader component

---
+++ src/view/components/main-content-header/index.tsx
@@ -0,0 +1,21 @@
+type TProps = {
+  title: string
+  description: string
+  children?: React.ReactNode
+}
+
+export default function MainContentHeader({
+  title,
+  description,
+  children,
+}: TProps) {
+  return (
+    <div className="flex flex-col justify-between gap-4 sm:flex-row sm:items-center sm:gap-0">
+      <div className="flex flex-col">
+        <h2 className="text-xl text-gray-600">{title}</h2>
+        <p className="text-sm font-light text-gray-400">{description}</p>
+      </div>
+      {children}
+    </div>
+  )
+}
