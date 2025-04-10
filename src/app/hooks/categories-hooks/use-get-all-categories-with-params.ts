e72e6fe361a5267a4ce12d251e3ce30ebfd8e04a
Matheus <matheusfariasdasneves@gmail.com>
2025-04-10 17:16:00 -0300

    feat: add useGetAllCategoriesWithParams custom hook

---
+++ src/app/hooks/categories-hooks/use-get-all-categories-with-params.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { CategoriesServices } from '@/app/services/categories-services'
+import type { IGetAllWithParamsProps } from '@/app/services/categories-services/interfaces'
+
+export function useGetAllCategoriesWithParams({
+  pageIndex,
+  searchTerm,
+}: IGetAllWithParamsProps) {
+  const { data: categories, isLoading } = useQuery({
+    queryKey: ['categories', pageIndex, searchTerm],
+    queryFn: () =>
+      CategoriesServices.getAllCategoriesWithParams({ pageIndex, searchTerm }),
+  })
+  return { categories, isLoading }
+}
