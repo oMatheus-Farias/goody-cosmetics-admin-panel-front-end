7fe5a563b6036598eadbf26e4a34f330b678b5aa
oMatheus-Farias <matheusfariasdasneves@gmail.com>
2025-04-18 12:02:13 -0300

    feat: add DeleteAlertDialog component and delete products function

---
+++ src/view/pages/admin/products/components/delete-alert-dialog.tsx
@@ -0,0 +1,98 @@
+import { useQueryClient } from '@tanstack/react-query'
+import { LoaderCircle, Trash2 } from 'lucide-react'
+import { useState } from 'react'
+import { toast } from 'sonner'
+
+import { env } from '@/app/configs/env-config'
+import { useDeleteProducts } from '@/app/hooks/products-hooks'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/view/components/ui/alert-dialog'
+import { Button } from '@/view/components/ui/button'
+
+type TProps = {
+  onOpenChange: (isOpen: boolean) => void
+  productId: string
+}
+
+export function DeleteAlertDialog({ onOpenChange, productId }: TProps) {
+  const queryClient = useQueryClient()
+  const [openAlertDialog, setOpenAlertDialog] = useState(false)
+  const { deleteProductsFn, isPending } = useDeleteProducts()
+
+  function handleOpenChange(isOpen: boolean) {
+    setOpenAlertDialog(isOpen)
+    onOpenChange(isOpen)
+  }
+  async function handleDelete(productId: string) {
+    try {
+      await deleteProductsFn(productId)
+      setOpenAlertDialog(false)
+      onOpenChange(false)
+      queryClient.invalidateQueries({ queryKey: ['products'] })
+      toast.success('Produto excluído com sucesso!')
+    } catch (error) {
+      if (env.VITE_NODE_ENV !== 'production') {
+        console.error('Error deleting product:', error)
+      }
+      toast.error('Erro ao excluir produto. Tente novamente mais tarde.')
+    }
+  }
+
+  return (
+    <AlertDialog open={openAlertDialog} onOpenChange={handleOpenChange}>
+      <AlertDialogTrigger asChild>
+        <Button
+          type="button"
+          variant="ghost"
+          size="icon"
+          aria-label="Excluir"
+          disabled={isPending}
+          onClick={(e) => {
+            e.stopPropagation()
+            setOpenAlertDialog(true)
+          }}
+          className="flex w-full justify-start px-2 hover:cursor-pointer"
+        >
+          {isPending ? <LoaderCircle className="animate-spin" /> : <Trash2 />}
+          Excluir
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle> Deseja mesmo excluir?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Essa ação não pode ser desfeita. Isso excluirá permanentemente esse
+            registro.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel
+            onClick={() => setOpenAlertDialog(false)}
+            className="hover:cursor-pointer"
+          >
+            Cancelar
+          </AlertDialogCancel>
+          <AlertDialogAction
+            disabled={isPending}
+            type="button"
+            aria-label="Excluir"
+            onClick={() => handleDelete(productId)}
+            className="bg-goodycosmetics-primary-500 hover:bg-goodycosmetics-primary-600 flex items-center gap-1 hover:cursor-pointer"
+          >
+            {isPending && <LoaderCircle className="animate-spin" />}
+            Continuar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
---
+++ src/view/pages/admin/products/components/index.ts
@@ -0,0 +1,3 @@
+export * from './delete-alert-dialog'
+export * from './product-action-options-dropdown-menu'
+export * from './products-table'
