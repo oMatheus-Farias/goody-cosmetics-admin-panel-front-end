ade666690954e5a1afc2354d52837267720a9602
Matheus <matheusfariasdasneves@gmail.com>
2025-05-09 11:19:15 -0300

    feat: ensure that the user is disconnected if there is no connection to the api

--- src/view/pages/_layouts/app-layout.tsx
+++ src/view/pages/_layouts/app-layout.tsx
@@ -2,6 +2,7 @@ import { useQueryClient } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
+import { env } from '@/app/configs/env-config'
 import { ROUTES_PATHS } from '@/app/constants/routes-paths'
 import { useAuth } from '@/app/hooks/auth-hooks/use-auth'
 import Footer from '@/view/components/footer'
@@ -11,13 +12,20 @@ import SidebarMenu from '@/view/components/sidebar-menu'
 
 export default function AppLayout() {
   const queryClient = useQueryClient()
-  const { signedIn, me } = useAuth()
+  const { signedIn, me, handleSignOut } = useAuth()
 
   useEffect(() => {
     if (signedIn && !me) {
-      queryClient.invalidateQueries({ queryKey: ['me'] })
+      try {
+        queryClient.invalidateQueries({ queryKey: ['me'] })
+      } catch (error) {
+        handleSignOut()
+        if (env.VITE_NODE_ENV !== 'production') {
+          console.error('Error invalidating queries:', error)
+        }
+      }
     }
-  }, [me, queryClient, signedIn])
+  }, [handleSignOut, me, queryClient, signedIn])
 
   if (!signedIn) {
     return <Navigate to={ROUTES_PATHS.LOGIN} replace={true} />
