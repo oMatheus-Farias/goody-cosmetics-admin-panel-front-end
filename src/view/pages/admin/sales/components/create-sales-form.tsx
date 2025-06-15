75acc5a244d5fa8340e4401466da12c6d7e57bf9
Matheus <matheusfariasdasneves@gmail.com>
2025-06-15 15:58:29 -0300

    fix: ensure that the unit price can be recorded with floating point

--- src/view/pages/admin/sales/components/create-sales-form.tsx
+++ src/view/pages/admin/sales/components/create-sales-form.tsx
@@ -11,6 +11,7 @@ import {
 } from 'lucide-react'
 import { useRef, useState } from 'react'
 import { Controller, useFieldArray, useForm } from 'react-hook-form'
+import { NumericFormat } from 'react-number-format'
 import { z } from 'zod'
 
 import { useGetAllProducts } from '@/app/hooks/products-hooks'
@@ -158,182 +159,183 @@ export function CreateSalesForm({ onOpenChange }: TProps) {
         <div className="flex flex-col gap-5">
           {fields.map((field, index) => {
             return (
-              <div key={field.id} className="flex items-center gap-2">
-                <Controller
-                  name={`items.${index}.productId`}
-                  control={form.control}
-                  render={({ field: { onChange, value, name } }) => (
-                    <div className="flex flex-1 flex-col gap-2">
-                      <Label
-                        htmlFor={`items.${index}.productId`}
-                        className="text-goodycosmetics-primary-700 text-xs font-normal uppercase"
-                      >
-                        Produto
-                      </Label>
-                      <div
-                        className={`${
-                          form.formState.errors.items?.[index]?.productId
-                            ? 'border-rose-500 has-focus-visible:ring-rose-300/80'
-                            : 'border-goodycosmetics-primary-400 has-focus:ring-goodycosmetics-primary-200'
-                        } flex h-12 w-full items-center rounded-[10px] border bg-white font-light has-focus:ring-[3px]`}
-                      >
-                        <Select
-                          name={name}
-                          onValueChange={onChange}
-                          value={value}
+              <div
+                key={field.id}
+                className="flex flex-col items-center gap-1 sm:flex-row sm:gap-2"
+              >
+                <div className="flex items-center gap-1 sm:gap-2">
+                  <Controller
+                    name={`items.${index}.productId`}
+                    control={form.control}
+                    render={({ field: { onChange, value, name } }) => (
+                      <div className="flex flex-1 flex-col gap-2">
+                        <Label
+                          htmlFor={`items.${index}.productId`}
+                          className="text-goodycosmetics-primary-700 text-xs font-normal uppercase"
                         >
-                          <SelectTrigger className="w-full border-none shadow-none placeholder:text-gray-400 focus-visible:border-none focus-visible:ring-0">
-                            <SelectValue
-                              defaultValue={''}
-                              placeholder="Selecione um produto"
-                            />
-                          </SelectTrigger>
-                          <SelectContent className="w-full bg-white">
-                            <SelectGroup>
-                              <SelectLabel className="uppercase">
-                                Produtos
-                              </SelectLabel>
-                              {isLoading ? (
-                                <SelectItem
-                                  value="loading"
-                                  disabled
-                                  className="font-light"
-                                >
-                                  <LoaderCircleIcon className="animate-spin" />
-                                  Carregando...
-                                </SelectItem>
-                              ) : products && products.length > 0 ? (
-                                products.map((product) => (
+                          Produto
+                        </Label>
+                        <div
+                          className={`${
+                            form.formState.errors.items?.[index]?.productId
+                              ? 'border-rose-500 has-focus-visible:ring-rose-300/80'
+                              : 'border-goodycosmetics-primary-400 has-focus:ring-goodycosmetics-primary-200'
+                          } flex h-12 w-full items-center rounded-[10px] border bg-white font-light has-focus:ring-[3px]`}
+                        >
+                          <Select
+                            name={name}
+                            onValueChange={onChange}
+                            value={value}
+                          >
+                            <SelectTrigger className="w-full border-none shadow-none placeholder:text-gray-400 focus-visible:border-none focus-visible:ring-0">
+                              <SelectValue
+                                defaultValue={''}
+                                placeholder="Selecione um produto"
+                              />
+                            </SelectTrigger>
+                            <SelectContent className="w-full bg-white">
+                              <SelectGroup>
+                                <SelectLabel className="uppercase">
+                                  Produtos
+                                </SelectLabel>
+                                {isLoading ? (
                                   <SelectItem
-                                    className="hover:bg-goodycosmetics-secondary-500 font-light text-gray-500 transition-all duration-200 ease-linear hover:cursor-pointer"
-                                    key={product.id}
-                                    value={product.id}
+                                    value="loading"
+                                    disabled
+                                    className="font-light"
                                   >
-                                    {product.name}
+                                    <LoaderCircleIcon className="animate-spin" />
+                                    Carregando...
                                   </SelectItem>
-                                ))
-                              ) : (
-                                <SelectItem
-                                  value="no-products"
-                                  disabled
-                                  className="font-light"
-                                >
-                                  Nenhum produto disponível.
-                                </SelectItem>
-                              )}
-                            </SelectGroup>
-                          </SelectContent>
-                        </Select>
+                                ) : products && products.length > 0 ? (
+                                  products.map((product) => (
+                                    <SelectItem
+                                      className="hover:bg-goodycosmetics-secondary-500 font-light text-gray-500 transition-all duration-200 ease-linear hover:cursor-pointer"
+                                      key={product.id}
+                                      value={product.id}
+                                    >
+                                      {product.name}
+                                    </SelectItem>
+                                  ))
+                                ) : (
+                                  <SelectItem
+                                    value="no-products"
+                                    disabled
+                                    className="font-light"
+                                  >
+                                    Nenhum produto disponível.
+                                  </SelectItem>
+                                )}
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                        </div>
+                        {form.formState.errors.items?.[index]?.productId && (
+                          <div className="flex items-center gap-2 font-light text-red-500">
+                            <AlertCircle className="max-h-2.5 min-h-2.5 max-w-2.5 min-w-2.5" />
+                            <FormMessage className="text-xs">
+                              {
+                                form.formState.errors.items?.[index]?.productId
+                                  ?.message
+                              }
+                            </FormMessage>
+                          </div>
+                        )}
                       </div>
-                      {form.formState.errors.items?.[index]?.productId && (
-                        <div className="flex items-center gap-2 font-light text-red-500">
-                          <AlertCircle className="max-h-2.5 min-h-2.5 max-w-2.5 min-w-2.5" />
-                          <FormMessage className="text-xs">
-                            {
-                              form.formState.errors.items?.[index]?.productId
-                                ?.message
-                            }
-                          </FormMessage>
-                        </div>
-                      )}
-                    </div>
-                  )}
-                />
-                <FormField
-                  name={`items.${index}.quantity`}
-                  control={form.control}
-                  render={() => (
-                    <FormItem>
-                      <FormControl>
-                        <div className="flex max-w-20 flex-col gap-2">
-                          <Label
-                            htmlFor={`items.${index}.quantity`}
-                            className="text-goodycosmetics-primary-700 text-xs font-normal uppercase"
-                          >
-                            Quantidade
-                          </Label>
-                          <div
-                            className={`${form.formState.errors.items?.[index]?.quantity ? 'border-rose-500 has-focus-visible:ring-rose-300/80' : 'border-goodycosmetics-primary-400 has-focus:ring-goodycosmetics-primary-200'} relative flex h-12 items-center rounded-[10px] border bg-white pr-3 font-light has-focus:ring-[3px]`}
-                          >
-                            <Input
-                              id={`items.${index}.quantity`}
-                              type="number"
-                              autoComplete="off"
-                              min={1}
-                              aria-label="Quantidade"
-                              {...form.register(`items.${index}.quantity`)}
-                              className="border-none shadow-none placeholder:text-gray-400 focus-visible:border-none focus-visible:ring-0"
+                    )}
+                  />
+                  <FormField
+                    name={`items.${index}.quantity`}
+                    control={form.control}
+                    render={() => (
+                      <FormItem>
+                        <FormControl>
+                          <div className="flex max-w-20 flex-col gap-2">
+                            <Label
+                              htmlFor={`items.${index}.quantity`}
+                              className="text-goodycosmetics-primary-700 text-xs font-normal uppercase"
+                            >
+                              Quantidade
+                            </Label>
+                            <div
+                              className={`${form.formState.errors.items?.[index]?.quantity ? 'border-rose-500 has-focus-visible:ring-rose-300/80' : 'border-goodycosmetics-primary-400 has-focus:ring-goodycosmetics-primary-200'} relative flex h-12 items-center rounded-[10px] border bg-white pr-3 font-light has-focus:ring-[3px]`}
+                            >
+                              <Input
+                                id={`items.${index}.quantity`}
+                                type="number"
+                                autoComplete="off"
+                                min={1}
+                                aria-label="Quantidade"
+                                {...form.register(`items.${index}.quantity`)}
+                                className="border-none shadow-none placeholder:text-gray-400 focus-visible:border-none focus-visible:ring-0"
+                              />
+                            </div>
+                          </div>
+                        </FormControl>
+                        {form.formState.errors.items?.[index]?.quantity && (
+                          <div className="flex max-w-20 items-center gap-2 font-light text-red-500">
+                            <AlertCircle className="max-h-2.5 min-h-2.5 max-w-2.5 min-w-2.5" />
+                            <FormMessage
+                              title={
+                                form.formState.errors.items?.[index]
+                                  ?.quantity &&
+                                form.formState.errors.items?.[index]?.quantity
+                                  .message
+                              }
+                              className="truncate text-xs"
                             />
                           </div>
-                        </div>
-                      </FormControl>
-                      {form.formState.errors.items?.[index]?.quantity && (
-                        <div className="flex max-w-20 items-center gap-2 font-light text-red-500">
-                          <AlertCircle className="max-h-2.5 min-h-2.5 max-w-2.5 min-w-2.5" />
-                          <FormMessage
-                            title={
-                              form.formState.errors.items?.[index]?.quantity &&
-                              form.formState.errors.items?.[index]?.quantity
-                                .message
-                            }
-                            className="truncate text-xs"
+                        )}
+                      </FormItem>
+                    )}
+                  />
+                  <Controller
+                    name={`items.${index}.unitPrice`}
+                    control={form.control}
+                    render={({ field: { onChange, value }, fieldState }) => (
+                      <div className="flex flex-col gap-2">
+                        <Label
+                          htmlFor={`items.${index}.unitPrice`}
+                          className="text-goodycosmetics-primary-700 text-xs font-normal uppercase"
+                        >
+                          R$ UNI
+                        </Label>
+                        <div
+                          className={`${form.formState.errors.items?.[index]?.unitPrice ? 'border-rose-500 has-focus-visible:ring-rose-300/80' : 'border-goodycosmetics-primary-400 has-focus:ring-goodycosmetics-primary-200'} flex h-12 w-full items-center rounded-[10px] border bg-white font-light has-focus:ring-[3px]`}
+                        >
+                          <NumericFormat
+                            id={`items.${index}.unitPrice`}
+                            allowNegative={false}
+                            decimalSeparator=","
+                            thousandSeparator="."
+                            defaultValue={value}
+                            placeholder="Digite o preço unitário"
+                            onValueChange={(values) => {
+                              onChange(values.floatValue ?? 0)
+                            }}
+                            onChange={() => {}}
+                            className="border-goodycosmetics-primary-400 flex h-12 w-full items-center rounded-[10px] border bg-white pl-3 font-light focus-visible:border-none focus-visible:ring-0 focus-visible:outline-none"
                           />
                         </div>
-                      )}
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  name={`items.${index}.unitPrice`}
-                  control={form.control}
-                  render={() => (
-                    <FormItem>
-                      <FormControl>
-                        <div className="flex max-w-20 flex-col gap-2">
-                          <Label
-                            htmlFor={`items.${index}.unitPrice`}
-                            className="text-goodycosmetics-primary-700 text-xs font-normal uppercase"
-                          >
-                            R$ (UNI)
-                          </Label>
-                          <div
-                            className={`${form.formState.errors.items?.[index]?.unitPrice ? 'border-rose-500 has-focus-visible:ring-rose-300/80' : 'border-goodycosmetics-primary-400 has-focus:ring-goodycosmetics-primary-200'} relative flex h-12 items-center rounded-[10px] border bg-white pr-3 font-light has-focus:ring-[3px]`}
-                          >
-                            <Input
-                              id={`items.${index}.unitPrice`}
-                              type="number"
-                              autoComplete="off"
-                              min={0}
-                              aria-label="Preço unitário"
-                              {...form.register(`items.${index}.unitPrice`)}
-                              className="border-none px-0.5 shadow-none placeholder:text-gray-400 focus-visible:border-none focus-visible:ring-0 sm:px-3"
-                            />
+                        {fieldState.error && (
+                          <div className="mt-1.5 flex items-center gap-2 font-light text-red-500">
+                            <AlertCircle className="max-h-2.5 min-h-2.5 max-w-2.5 min-w-2.5" />
+                            <span className="text-xs text-rose-500">
+                              {fieldState.error.message}
+                            </span>
                           </div>
-                        </div>
-                      </FormControl>
-                      {form.formState.errors.items?.[index]?.unitPrice && (
-                        <div className="flex max-w-20 items-center gap-2 font-light text-red-500">
-                          <AlertCircle className="max-h-2.5 min-h-2.5 max-w-2.5 min-w-2.5" />
-                          <FormMessage
-                            title={
-                              form.formState.errors.items?.[index]?.unitPrice &&
-                              form.formState.errors.items?.[index]?.unitPrice
-                                .message
-                            }
-                            className="truncate text-xs"
-                          />
-                        </div>
-                      )}
-                    </FormItem>
-                  )}
-                />
+                        )}
+                      </div>
+                    )}
+                  />
+                </div>
                 <Button
                   type="button"
                   variant="ghost"
                   title="Remover item"
                   aria-label="Remover item"
                   onClick={() => remove(index)}
-                  className={`${index === 0 ? 'hidden' : ''} mt-6 h-12 w-10 rounded-[10px] border border-rose-500 text-white transition-all duration-150 ease-linear hover:cursor-pointer hover:bg-rose-50`}
+                  className={`${index === 0 ? 'hidden' : ''} mt-2 h-12 w-full rounded-[10px] border border-rose-500 text-white transition-all duration-150 ease-linear hover:cursor-pointer hover:bg-rose-50 sm:mt-6 sm:w-8`}
                 >
                   <Trash2 className="w-3.5 text-rose-500" />
                 </Button>
